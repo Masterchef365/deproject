e185a848a10f93f669621a5c893af54fbee667f9
Masterchef365 <duncan.freeman1@gmail.com>
2022-11-20 18:03:06 -0800

    Cargo fmt

--- src/bin/blobs.rs
+++ src/bin/blobs.rs
@@ -5,13 +5,13 @@ use deproject::fluid::FluidSim;
 use deproject::plane::Plane;
 use deproject::projector::{load_projector_model, ProjectorModel};
 use deproject::{pointcloud_fast, Vertex};
-use glow::{HasContext, Context, NativeBuffer, VertexArray, Program};
+use glow::{Context, HasContext, NativeBuffer, Program, VertexArray};
 use glutin::window::Fullscreen;
 use nalgebra::{Point2, Point3, Vector2};
 use rand::{distributions::Uniform, prelude::Distribution, Rng};
 use std::fs::File;
 use std::path::PathBuf;
-use std::sync::mpsc::{Sender, channel, Receiver};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::time::Duration;
 
 use realsense_rust::{
@@ -38,7 +38,12 @@ struct FloorProgram {
 }
 
 impl FloorProgram {
-    pub fn new(gl: &Context, model: ProjectorModel, tracking_rx: Receiver<BlobTrackerDelta>, plane: Plane) -> Result<Self> {
+    pub fn new(
+        gl: &Context,
+        model: ProjectorModel,
+        tracking_rx: Receiver<BlobTrackerDelta>,
+        plane: Plane,
+    ) -> Result<Self> {
         unsafe {
             let program = gl.create_program().expect("Cannot create program");
 
@@ -129,7 +134,6 @@ impl FloorProgram {
                 bytemuck::cast_slice(&model),
             );
 
-
             Ok(Self {
                 line_verts,
                 line_buf,
@@ -170,7 +174,11 @@ impl FloorProgram {
 
         // Transform line vertices from plane space into camera space
         for v in &mut self.line_verts {
-            v.pos = *self.plane.from_planespace(Point3::from(v.pos).xzy()).coords.as_ref();
+            v.pos = *self
+                .plane
+                .from_planespace(Point3::from(v.pos).xzy())
+                .coords
+                .as_ref();
         }
 
         unsafe {
@@ -194,8 +202,6 @@ impl FloorProgram {
 
         //gl.uniform_3_f32(loc.as_ref(), x, y, z);
         //gl.draw_arrays(glow::TRIANGLES, 0, 3);
-
-
     }
 
     fn destroy(&mut self, gl: &Context) {
@@ -262,9 +268,7 @@ fn main() -> Result<()> {
                         window.resize(*physical_size);
                         gl.viewport(0, 0, physical_size.width as _, physical_size.height as _);
                     }
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit
-                    }
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                     _ => (),
                 },
                 _ => (),
@@ -321,8 +325,8 @@ fn tracking_thread(delta: Sender<BlobTrackerDelta>, plane: Plane) -> Result<()> 
         plane_points.clear();
         plane_points.extend(
             pcld.drain(..)
-            .filter(|&p| dist_range.contains(&plane.distance(Point3::from(p))))
-            .map(|p| plane.to_planespace(Point3::from(p)).xz()),
+                .filter(|&p| dist_range.contains(&plane.distance(Point3::from(p))))
+                .map(|p| plane.to_planespace(Point3::from(p)).xz()),
         );
 
         tracker.track(&plane_points);
@@ -393,280 +397,284 @@ impl BlobTracker {
             }
         }
     }
-    }
+}
 
 #[derive(Clone)]
-    struct BlobBoxes {
-        cell_width: f32,
-        min_blob_area: i64,
-        max_blob_area: i64,
-        boxes: AHashMap<Point2<i32>, usize>,
-        tmp: AHashMap<Point2<i32>, usize>,
-        bounds: Vec<Rect2>,
+struct BlobBoxes {
+    cell_width: f32,
+    min_blob_area: i64,
+    max_blob_area: i64,
+    boxes: AHashMap<Point2<i32>, usize>,
+    tmp: AHashMap<Point2<i32>, usize>,
+    bounds: Vec<Rect2>,
+}
+
+impl BlobBoxes {
+    pub fn new(cell_width: f32, min_blob_area: i64, max_blob_area: i64) -> Self {
+        Self {
+            cell_width,
+            min_blob_area,
+            max_blob_area,
+            boxes: Default::default(),
+            tmp: Default::default(),
+            bounds: Default::default(),
+        }
     }
 
-    impl BlobBoxes {
-        pub fn new(cell_width: f32, min_blob_area: i64, max_blob_area: i64) -> Self {
-            Self {
-                cell_width,
-                min_blob_area,
-                max_blob_area,
-                boxes: Default::default(),
-                tmp: Default::default(),
-                bounds: Default::default(),
-            }
+    pub fn insert(&mut self, points: &[Point2<f32>]) {
+        self.clear();
+        self.quantize(points);
+        self.find_blobs();
+    }
+
+    fn clear(&mut self) {
+        self.boxes.clear();
+        self.bounds.clear();
+        self.tmp.clear();
+    }
+
+    fn quantize(&mut self, points: &[Point2<f32>]) {
+        for point in points {
+            let key = point / self.cell_width;
+            let key = Point2::new(key.x as i32, key.y as i32);
+            *self.boxes.entry(key).or_insert(0) += 1;
         }
+    }
 
-        pub fn insert(&mut self, points: &[Point2<f32>]) {
-            self.clear();
-            self.quantize(points);
-            self.find_blobs();
-        }
+    fn find_blobs(&mut self) {
+        let mut search_buf = vec![];
+        self.tmp.clear();
+        self.tmp.extend(self.boxes.iter());
+        self.bounds.clear();
 
-        fn clear(&mut self) {
-            self.boxes.clear();
-            self.bounds.clear();
-            self.tmp.clear();
-        }
+        while let Some(&seed) = self.tmp.keys().next() {
+            let (seed, _count) = self.tmp.remove_entry(&seed).unwrap();
+            search_buf.push(seed);
+            let mut bbox = Rect2::new(seed);
 
-        fn quantize(&mut self, points: &[Point2<f32>]) {
-            for point in points {
-                let key = point / self.cell_width;
-                let key = Point2::new(key.x as i32, key.y as i32);
-                *self.boxes.entry(key).or_insert(0) += 1;
-            }
-        }
+            while let Some(part) = search_buf.pop() {
+                let offsets = [
+                    Vector2::new(1, 0),
+                    Vector2::new(-1, 0),
+                    Vector2::new(0, 1),
+                    Vector2::new(0, -1),
+                ];
 
-        fn find_blobs(&mut self) {
-            let mut search_buf = vec![];
-            self.tmp.clear();
-            self.tmp.extend(self.boxes.iter());
-            self.bounds.clear();
-
-            while let Some(&seed) = self.tmp.keys().next() {
-                let (seed, _count) = self.tmp.remove_entry(&seed).unwrap();
-                search_buf.push(seed);
-                let mut bbox = Rect2::new(seed);
-
-                while let Some(part) = search_buf.pop() {
-                    let offsets = [
-                        Vector2::new(1, 0),
-                        Vector2::new(-1, 0),
-                        Vector2::new(0, 1),
-                        Vector2::new(0, -1),
-                    ];
-
-                    for off in offsets {
-                        let subject = part + off;
-                        if let Some((s, _)) = self.tmp.remove_entry(&subject) {
-                            search_buf.push(s);
-                            bbox.insert(s);
-                        }
+                for off in offsets {
+                    let subject = part + off;
+                    if let Some((s, _)) = self.tmp.remove_entry(&subject) {
+                        search_buf.push(s);
+                        bbox.insert(s);
                     }
                 }
+            }
 
-                if (self.min_blob_area..=self.max_blob_area).contains(&bbox.area()) {
-                    self.bounds.push(bbox);
-                }
+            if (self.min_blob_area..=self.max_blob_area).contains(&bbox.area()) {
+                self.bounds.push(bbox);
             }
         }
     }
+}
 
 #[derive(Copy, Clone, Debug)]
-    struct Rect2 {
-        min: Point2<i32>,
-        max: Point2<i32>,
+struct Rect2 {
+    min: Point2<i32>,
+    max: Point2<i32>,
+}
+
+impl Rect2 {
+    pub fn new(pt: Point2<i32>) -> Self {
+        Self { min: pt, max: pt }
     }
 
-    impl Rect2 {
-        pub fn new(pt: Point2<i32>) -> Self {
-            Self { min: pt, max: pt }
-        }
+    pub fn insert(&mut self, pt: Point2<i32>) {
+        self.min.x = self.min.x.min(pt.x);
+        self.min.y = self.min.y.min(pt.y);
 
-        pub fn insert(&mut self, pt: Point2<i32>) {
-            self.min.x = self.min.x.min(pt.x);
-            self.min.y = self.min.y.min(pt.y);
-
-            self.max.x = self.max.x.max(pt.x);
-            self.max.y = self.max.y.max(pt.y);
-        }
-
-        pub fn area(&self) -> i64 {
-            let s = self.max - self.min;
-            let s = s.cast::<i64>();
-            s.x * s.y
-        }
-
-        pub fn center(&self) -> Point2<i32> {
-            self.min + (self.max - self.min) / 2
-        }
-
-        pub fn intersects(&self, other: &Self) -> bool {
-            !(self.max.x < other.min.x
-                || other.max.x < self.min.x
-                || self.max.y < other.min.y
-                || other.max.y < self.min.y)
-        }
+        self.max.x = self.max.x.max(pt.x);
+        self.max.y = self.max.y.max(pt.y);
     }
 
-    fn blob_box_lines(lines: &mut Vec<Vertex>, blob_boxes: &BlobBoxes) {
-        let mut push_vert = |pt: Point2<f32>, color: [f32; 3]| {
-            lines.push(Vertex {
-                pos: [pt.x, pt.y, 0.0],
-                color,
-            })
-        };
-
-        let mut draw_box = |min: Point2<i32>, max: Point2<i32>, color: [f32; 3]| {
-            let bw = blob_boxes.cell_width;
-            let tl: Point2<f32> = min.cast() * bw;
-            let br: Point2<f32> = max.cast() * bw + Vector2::from_element(bw);
-
-            let tr = Point2::new(br.x, tl.y);
-            let bl = Point2::new(tl.x, br.y);
-
-            push_vert(tl, color);
-            push_vert(bl, color);
-
-            push_vert(tl, color);
-            push_vert(tr, color);
-
-            push_vert(tr, color);
-            push_vert(br, color);
-
-            push_vert(bl, color);
-            push_vert(br, color);
-        };
-
-        for (&tl, _) in &blob_boxes.boxes {
-            draw_box(tl, tl, [1., 0., 0.]);
-        }
-
-        for rect in &blob_boxes.bounds {
-            draw_box(rect.min, rect.max, [1., 1., 0.]);
-        }
+    pub fn area(&self) -> i64 {
+        let s = self.max - self.min;
+        let s = s.cast::<i64>();
+        s.x * s.y
     }
 
-    fn draw_delta(lines: &mut Vec<Vertex>, delta: &BlobTrackerDelta, cell_width: f32) {
-        let mut push_vertex = |pt: Point2<f32>, color: [f32; 3]| {
-            lines.push(Vertex {
-                pos: [pt.x, pt.y, 0.0],
-                color,
-            })
-        };
-
-        for (pt, v) in delta {
-            let pt = pt.cast::<f32>() * cell_width;
-            push_vertex(pt, [0.; 3]);
-            push_vertex(pt + *v, [1.; 3]);
-        }
+    pub fn center(&self) -> Point2<i32> {
+        self.min + (self.max - self.min) / 2
     }
 
-    fn draw_velocity_lines(lines: &mut Vec<Vertex>, (u, v): (&Array2D<f32>, &Array2D<f32>), z: f32) {
-        let mut push_vertex = |pt: Point2<f32>, color: [f32; 3]| {
-            lines.push(Vertex {
-                pos: [pt.x, pt.y, 0.0],
-                color,
-            })
-        };
+    pub fn intersects(&self, other: &Self) -> bool {
+        !(self.max.x < other.min.x
+            || other.max.x < self.min.x
+            || self.max.y < other.min.y
+            || other.max.y < self.min.y)
+    }
+}
 
-        let cell_width = 2. / u.width() as f32;
-        let cell_height = 2. / u.height() as f32;
+fn blob_box_lines(lines: &mut Vec<Vertex>, blob_boxes: &BlobBoxes) {
+    let mut push_vert = |pt: Point2<f32>, color: [f32; 3]| {
+        lines.push(Vertex {
+            pos: [pt.x, pt.y, 0.0],
+            color,
+        })
+    };
 
-        for i in 0..u.width() {
-            let i_frac = (i as f32 / u.width() as f32) * 2. - 1.;
-            for j in 0..u.height() {
-                let j_frac = (j as f32 / u.height() as f32) * 2. - 1.;
+    let mut draw_box = |min: Point2<i32>, max: Point2<i32>, color: [f32; 3]| {
+        let bw = blob_boxes.cell_width;
+        let tl: Point2<f32> = min.cast() * bw;
+        let br: Point2<f32> = max.cast() * bw + Vector2::from_element(bw);
 
-                let u = u[(i, j)];
-                let v = v[(i, j)];
+        let tr = Point2::new(br.x, tl.y);
+        let bl = Point2::new(tl.x, br.y);
 
-                let speed = (u.powf(2.) + v.powf(2.)).sqrt();
+        push_vert(tl, color);
+        push_vert(bl, color);
 
-                let color = [speed * 80.; 3];
+        push_vert(tl, color);
+        push_vert(tr, color);
 
-                let tail = Point2::new(i_frac + cell_width / 2., j_frac + cell_height / 2.);
-                push_vertex(tail, [0.; 3]);
+        push_vert(tr, color);
+        push_vert(br, color);
 
-                let len = cell_height * 2. / speed;
-                let tip = tail + Vector2::new(u, v) * len;
-                push_vertex(tip, color);
-            }
-        }
+        push_vert(bl, color);
+        push_vert(br, color);
+    };
+
+    for (&tl, _) in &blob_boxes.boxes {
+        draw_box(tl, tl, [1., 0., 0.]);
     }
 
-    fn delta_to_fluid((u, v): (&mut Array2D<f32>, &mut Array2D<f32>), delta: &BlobTrackerDelta, cell_width: f32) {
+    for rect in &blob_boxes.bounds {
+        draw_box(rect.min, rect.max, [1., 1., 0.]);
+    }
+}
+
+fn draw_delta(lines: &mut Vec<Vertex>, delta: &BlobTrackerDelta, cell_width: f32) {
+    let mut push_vertex = |pt: Point2<f32>, color: [f32; 3]| {
+        lines.push(Vertex {
+            pos: [pt.x, pt.y, 0.0],
+            color,
+        })
+    };
+
+    for (pt, v) in delta {
+        let pt = pt.cast::<f32>() * cell_width;
+        push_vertex(pt, [0.; 3]);
+        push_vertex(pt + *v, [1.; 3]);
+    }
+}
+
+fn draw_velocity_lines(lines: &mut Vec<Vertex>, (u, v): (&Array2D<f32>, &Array2D<f32>), z: f32) {
+    let mut push_vertex = |pt: Point2<f32>, color: [f32; 3]| {
+        lines.push(Vertex {
+            pos: [pt.x, pt.y, 0.0],
+            color,
+        })
+    };
+
+    let cell_width = 2. / u.width() as f32;
+    let cell_height = 2. / u.height() as f32;
+
+    for i in 0..u.width() {
+        let i_frac = (i as f32 / u.width() as f32) * 2. - 1.;
+        for j in 0..u.height() {
+            let j_frac = (j as f32 / u.height() as f32) * 2. - 1.;
+
+            let u = u[(i, j)];
+            let v = v[(i, j)];
+
+            let speed = (u.powf(2.) + v.powf(2.)).sqrt();
+
+            let color = [speed * 80.; 3];
+
+            let tail = Point2::new(i_frac + cell_width / 2., j_frac + cell_height / 2.);
+            push_vertex(tail, [0.; 3]);
+
+            let len = cell_height * 2. / speed;
+            let tip = tail + Vector2::new(u, v) * len;
+            push_vertex(tip, color);
+        }
+    }
+}
+
+fn delta_to_fluid(
+    (u, v): (&mut Array2D<f32>, &mut Array2D<f32>),
+    delta: &BlobTrackerDelta,
+    cell_width: f32,
+) {
+    let w = u.width() as f32;
+    let h = u.height() as f32;
+
+    for (pt, vt) in delta {
+        let pt = (pt.cast::<f32>() * cell_width) / 2. + Vector2::from_element(0.5);
+        let i = (pt.x * w).clamp(0., w - 1.) as usize;
+        let j = (pt.y * h).clamp(0., h - 1.) as usize;
+
+        u[(i, j)] += vt.x;
+        v[(i, j)] += vt.y;
+    }
+}
+
+struct Floaters {
+    parts: Vec<Point2<f32>>,
+    last: Vec<Point2<f32>>,
+    mask: Vec<bool>,
+}
+
+impl Floaters {
+    pub fn new(n: usize) -> Self {
+        let parts = vec![Point2::new(-10., -10.); n];
+        let last = parts.clone();
+        let mask = vec![false; n];
+
+        Self { parts, last, mask }
+    }
+
+    pub fn step(&mut self, (u, v): (&Array2D<f32>, &Array2D<f32>), dt: f32) {
+        std::mem::swap(&mut self.parts, &mut self.last);
+
+        let mut rng = rand::thread_rng();
+        let unif = Uniform::new(-1., 1.);
+
+        let kill_rate = 0.0001;
+
         let w = u.width() as f32;
         let h = u.height() as f32;
+        for ((part, last), mask) in self.parts.iter_mut().zip(&self.last).zip(&mut self.mask) {
+            let in_bounds = part.x >= -1. && part.x <= 1. && part.y >= -1. && part.y <= 1.;
 
-        for (pt, vt) in delta {
-            let pt = (pt.cast::<f32>() * cell_width) / 2. + Vector2::from_element(0.5);
-            let i = (pt.x * w).clamp(0., w - 1.) as usize;
-            let j = (pt.y * h).clamp(0., h - 1.) as usize;
+            let alive = in_bounds && rng.gen_bool(1. - kill_rate);
 
-            u[(i, j)] += vt.x;
-            v[(i, j)] += vt.y;
-        }
-    }
-
-    struct Floaters {
-        parts: Vec<Point2<f32>>,
-        last: Vec<Point2<f32>>,
-        mask: Vec<bool>,
-    }
-
-    impl Floaters {
-        pub fn new(n: usize) -> Self {
-            let parts = vec![Point2::new(-10., -10.); n];
-            let last = parts.clone();
-            let mask = vec![false; n];
-
-            Self { parts, last, mask }
-        }
-
-        pub fn step(&mut self, (u, v): (&Array2D<f32>, &Array2D<f32>), dt: f32) {
-            std::mem::swap(&mut self.parts, &mut self.last);
-
-            let mut rng = rand::thread_rng();
-            let unif = Uniform::new(-1., 1.);
-
-            let kill_rate = 0.0001;
-
-            let w = u.width() as f32;
-            let h = u.height() as f32;
-            for ((part, last), mask) in self.parts.iter_mut().zip(&self.last).zip(&mut self.mask) {
-                let in_bounds = part.x >= -1. && part.x <= 1. && part.y >= -1. && part.y <= 1.;
-
-                let alive = in_bounds && rng.gen_bool(1. - kill_rate);
-
-                if alive {
-                    let pt = part.cast::<f32>() / 2. + Vector2::from_element(0.5);
-                    let i = (pt.x * w).clamp(0., w - 1.) as usize;
-                    let j = (pt.y * h).clamp(0., h - 1.) as usize;
-                    let uv = Vector2::new(u[(i, j)], v[(i, j)]);
-                    *part = last + uv * dt;
-                } else {
-                    *part = Point2::new(unif.sample(&mut rng), unif.sample(&mut rng));
-                }
-
-                *mask = alive;
+            if alive {
+                let pt = part.cast::<f32>() / 2. + Vector2::from_element(0.5);
+                let i = (pt.x * w).clamp(0., w - 1.) as usize;
+                let j = (pt.y * h).clamp(0., h - 1.) as usize;
+                let uv = Vector2::new(u[(i, j)], v[(i, j)]);
+                *part = last + uv * dt;
+            } else {
+                *part = Point2::new(unif.sample(&mut rng), unif.sample(&mut rng));
             }
+
+            *mask = alive;
         }
+    }
 
-        pub fn draw(&self, lines: &mut Vec<Vertex>) {
-            let mut push_vertex = |pt: Point2<f32>, color: [f32; 3]| {
-                lines.push(Vertex {
-                    pos: [pt.x, pt.y, 0.0],
-                    color,
-                })
-            };
+    pub fn draw(&self, lines: &mut Vec<Vertex>) {
+        let mut push_vertex = |pt: Point2<f32>, color: [f32; 3]| {
+            lines.push(Vertex {
+                pos: [pt.x, pt.y, 0.0],
+                color,
+            })
+        };
 
-            for ((part, last), mask) in self.parts.iter().zip(&self.last).zip(&self.mask) {
-                if *mask {
-                    let d = part - last;
-                    push_vertex(*last, [1.; 3]);
-                    push_vertex(*part + d * 5., [1.; 3]);
-                }
+        for ((part, last), mask) in self.parts.iter().zip(&self.last).zip(&self.mask) {
+            if *mask {
+                let d = part - last;
+                push_vertex(*last, [1.; 3]);
+                push_vertex(*part + d * 5., [1.; 3]);
             }
         }
     }
+}
